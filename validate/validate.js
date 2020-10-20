fe53bd98d6d2229db92011e313a92c62223c3560
Danielsear <danielandradeseara@hotmail.com>
2020-10-20 18:11:01 -0300

    validating fields senha e email

--- validate/validate.js
+++ validate/validate.js
@@ -6,6 +6,7 @@ function validaSenha(event) {
   //console.log("entrou");
   let campos = document.querySelectorAll("span.error");
   let campo = campos[1];
+
   if (
     (NumberInput.length < 4 || NumberInput.length > 12) &&
     NumberInput.length > 0
@@ -13,7 +14,30 @@ function validaSenha(event) {
     //console.log(campo[1]);
     // console.log(campo[1].classList.add("active"));
     campo.classList.add("active");
-    campo.innerHTML = "Campo invalido";
+    campo.innerHTML = "A senha deve ser >4 ou <12 dígitos";
+  } else {
+    const error = event.target.validity;
+    testError(error, 1, "senha");
+  }
+}
+
+function validaEmail(event) {
+  console.log(event.target.validity);
+  const error = event.target.validity;
+  testError(error, 0, "email");
+}
+
+function testError(error, indece, name) {
+  let campos = document.querySelectorAll("span.error");
+  let campo = campos[indece];
+  console.log(error.typeMismatch);
+
+  if (error.typeMismatch === true) {
+    campo.classList.add("active");
+    campo.innerHTML = name + " inválido";
+  } else if (error.valueMissing === true) {
+    campo.classList.add("active");
+    campo.innerHTML = name + " obrigatório";
   } else {
     campo.classList.remove("active");
     campo.innerHTML = "";
